9f30885df1755dca885d0ffe87a8945ea3bce401
Jose Ramon Batiller <jb17953@my.bristol.ac.uk>
2017-10-11 12:59:18 +0100

    changed number 1 to return an array of x given an array of t

--- number1.py
+++ number1.py
@@ -10,6 +10,9 @@ Created on Wed Oct 11 10:46:16 2017
 #solve ODE dxdt=x
 import numpy as np
 import scipy as sp
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
 
 def dxdt(x,t):
     dxdt=x
@@ -40,26 +43,41 @@ def solve_to(xstart,tstart,tend,deltat=0.01):
     """
     xnow=xstart
     tnow=tstart
+    fsteps=(tend-tstart)/deltat
+    steps=int((tend-tstart)/deltat)
+    if(fsteps-steps>=0.000005):
+        steps+=1
+          
+    deltat=(tend-tstart)/steps
+    for step in range(steps):
+        [xnow, tnow] =euler_step(xnow,tnow, deltat)
+    return xnow
     
-    while tnow<tend:
+    """while tnow<tend:
         #check size
         if tend-tnow<=deltat:
             deltat=tend-tnow
         [xnow, tnow] =euler_step(xnow,tnow, deltat)
-    return [xnow, tnow]
+    return xnow"""
     
-def solve_ode():
-    # this returns plottable points
-    x_list=np.array([[xstart,tstart]])
-    x_list.reshape(2,1)
+def solve_ode(x0, t, deltat=0.01):
+    # t is a bloddy array!!!
+    x_list=np.array([solve_to(x0,t0,t1) for t1 in t ])
+    #having t0 here is irritating me somehow
+    return x_list
     
-
-
-
 x0=1
 t0=0
 
+t=np.arange(10)
+x=solve_ode(x0,t)
 
+plt.plot(t,x)
+
+plt.plot(t, odeint(dxdt,x0,t))
+
+
+plt.plot(t,x, deltat=0.00001)
     
     
     
